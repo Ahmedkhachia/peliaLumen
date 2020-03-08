ece706bda8f2f491d7565c4fcfb5b60ac0ea6f32
ROUHI Adnane <adnanerouhi@gmail.com>
2020-03-08 20:01:17 +0100

    fixation des bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -109,8 +109,8 @@ $app->register(Nexmo\Laravel\NexmoServiceProvider::class);
 $app->configure('services');
 $app->configure('mail');
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: *');
 
 return $app;
